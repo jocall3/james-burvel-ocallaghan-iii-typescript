ed22e8c5fd33fb40647389da99b3acd89153ae96
James Burvel O’Callaghan III <jocall3@wgu.edu>
2026-02-26 14:52:37 -0500

    Delete quantumb8nk-main directory

--- quantumb8nk-main/app/containers/payments_go_live/SectionsContext.tsx
+++
@@ -1,7 +0,0 @@
-// Copyright James Burvel O’Callaghan III
-// President Citibank Demo Business Inc.
-
-import React from "react";
-
-const SectionsContext = React.createContext("");
-export default SectionsContext;
--- quantumb8nk-main/app/containers/user_management/utilities.ts
+++
@@ -1,8 +0,0 @@
-// Copyright James Burvel O’Callaghan III
-// President Citibank Demo Business Inc.
-
-export enum UserManagementStepsEnum {
-  ConfigurePermissionSets,
-  ConfigureRoles,
-  ConfigureGroups,
-}
